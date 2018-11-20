852df1f599aa6ba83518ad0d559e13cf121ead53
Tomohiro <gpuppur@gmail.com>
2018-11-21 08:37:05 +0900

    Install nim without using choosenim

--- build.sh
+++ build.sh
@@ -1,5 +1,43 @@
-curl https://nim-lang.org/choosenim/init.sh -sSf > init.sh
-sh init.sh -y
-export PATH=/opt/buildhome/.nimble/bin:$PATH
+#!/bin/bash
+
+NIM_REQUIRED_VER="0.18.0"
+: ${NETLIFY_BUILD_BASE="/opt/buildhome"}
+NETLIFY_CACHE_DIR="$NETLIFY_BUILD_BASE/cache"
+NIM_INSTALL_DIR="$NETLIFY_CACHE_DIR/nim"
+
+install_nim() {
+  local nimver = $NIM_REQUIRED_VER
+
+  rm -rf $NIM_INSTALL_DIR
+
+  wget -N https://nim-lang.org/download/nim-$nimver.tar.xz
+  wget -N https://nim-lang.org/download/nim-$nimver.tar.xz.sha256
+
+  sha256sum -c nim-$nimver.tar.xz.sha256
+
+  if [ ! $? = 0 ]; then
+    echo "Failed to download nim source code"
+  fi
+
+  tar xf nim-$nimver.tar.xz
+  pushd nim-$nimver
+  sh build.sh
+  bin/nim c koch
+  ./koch tools
+  ./koch install $NIM_INSTALL_DIR
+  popd
+}
+
+install_nim_check() {
+  if command -v nim && nim e checknimver.nims $NIM_REQUIRED_VER
+  then
+    echo skip installing nim
+  else
+    install_nim
+  fi
+}
+
+export PATH=$NIM_INSTALL_DIR/bin:$PATH
+install_nim_check
 nim -v
 nim c -r test.nim
