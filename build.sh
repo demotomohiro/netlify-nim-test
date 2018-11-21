2bcfc8dfe07fc097751950b467ed51deb38740ce
demotomohiro <gpuppur@gmail.com>
2018-11-22 03:20:44 +0900

    Move install_nim and install_nim_check to other file

--- build.sh
+++ build.sh
@@ -3,44 +3,9 @@
 set -e
 set -u
 
-NIM_REQUIRED_VER="0.19.0"
-: ${NETLIFY_BUILD_BASE="/opt/buildhome"}
-NETLIFY_CACHE_DIR="$NETLIFY_BUILD_BASE/cache"
-NIM_INSTALL_DIR="$NETLIFY_CACHE_DIR"
+src_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+source "$src_dir/scripts/installnim.sh"
 
-install_nim() {
-  local nimver=$NIM_REQUIRED_VER
-
-  rm -rf $NIM_INSTALL_DIR/nim
-
-  wget -N -nv https://nim-lang.org/download/nim-$nimver.tar.xz
-  wget -N -nv https://nim-lang.org/download/nim-$nimver.tar.xz.sha256
-
-  sha256sum -c nim-$nimver.tar.xz.sha256
-
-  if [ ! $? = 0 ]; then
-    echo "Failed to download nim source code"
-  fi
-
-  tar xf nim-$nimver.tar.xz
-  pushd nim-$nimver
-  sh build.sh
-  bin/nim c koch
-  ./koch tools
-  ./koch install $NIM_INSTALL_DIR
-  popd
-}
-
-install_nim_check() {
-  if command -v nim && nim e checknimver.nims $NIM_REQUIRED_VER
-  then
-    echo skip installing nim
-  else
-    install_nim
-  fi
-}
-
-export PATH=$NIM_INSTALL_DIR/nim/bin:$PATH
-install_nim_check
+install_nim_check "0.19.0"
 nim -v
-nim c -r test.nim
+nim c -r "$src_dir/test.nim"
