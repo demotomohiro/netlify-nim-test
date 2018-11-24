f61796d364fcac75d11f161fca5ae33a0d2956e7
demotomohiro <gpuppur@gmail.com>
2018-11-24 15:05:25 +0900

    Fix path in build.sh

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ src_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 source "$src_dir/scripts/installnim.sh"
 
 install_nim_check "0.19.0"
-cd "$src_dir"
+cd "$src_dir/src"
 nim c -r -d:release "$src_dir/src/sampleblog.nim"
