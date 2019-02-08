bb3312f010e8ac8be296ca082efb4759f9cbcd2e
Tomohiro <gpuppur@gmail.com>
2019-02-09 03:01:46 +0900

    Use Nim-0.19.4

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ set -u
 src_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 source "$src_dir/scripts/installnim.sh"
 
-install_nim_check "0.19.0"
+install_nim_check "0.19.4"
 nim -v
 nim c -r "$src_dir/test.nim"
